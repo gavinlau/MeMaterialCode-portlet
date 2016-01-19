3cbc03e3c14d3a7fbed29a4bf642843d5039fae5
gavinlau <logicbiz@qq.com>
2016-01-19 15:22:06 +0800

    updated

---
+++ docroot/WEB-INF/src/com/j2eecn/mc/MaterialCodePortlet.java
@@ -0,0 +1,11 @@
+package com.j2eecn.mc;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class MaterialCodePortlet
+ */
+public class MaterialCodePortlet extends MVCPortlet {
+ 
+
+}
